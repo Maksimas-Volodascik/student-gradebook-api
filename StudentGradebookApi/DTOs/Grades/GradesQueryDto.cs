21d0587495d742b8e99e2be4ca02bc2eea6a4210
max <maksimas.volodascik@gmail.com>
2026-04-06 17:23:51 +0300

    feat(api): add json property names on grades query dto for redis caching

--- StudentGradebookApi/DTOs/Grades/GradesQueryDto.cs
+++ StudentGradebookApi/DTOs/Grades/GradesQueryDto.cs
@@ -1,14 +1,19 @@
 ﻿using StudentGradebookApi.DTOs.SharedDto;
+using System.Text.Json.Serialization;
 
 namespace StudentGradebookApi.DTOs.Grades
 {
     public class GradesQueryDto : QueryDto
     {
-        //Filters
+        [JsonPropertyName("firstName")]
         public string? FirstName { get; set; }
+        [JsonPropertyName("lastName")]
         public string? LastName { get; set; }
+        [JsonPropertyName("classSubjectId")]
         public int ClassSubjectId { get; set; } = 1;
+        [JsonPropertyName("gradingYear")]
         public int GradingYear { get; set; } = DateTime.Now.Year;
+        [JsonPropertyName("gradingMonth")]
         public int GradingMonth { get; set; } = DateTime.Now.Month;
     }
 }
