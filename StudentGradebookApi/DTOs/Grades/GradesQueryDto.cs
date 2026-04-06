ec1ed3520780fae241d912b38310dc8d68dc8102
max <maksimas.volodascik@gmail.com>
2026-04-06 17:36:18 +0300

    feat(api): add json property names on grades query dto for redis caching

--- StudentGradebookApi/DTOs/Grades/GradesQueryDto.cs
+++ StudentGradebookApi/DTOs/Grades/GradesQueryDto.cs
@@ -5,15 +5,10 @@ namespace StudentGradebookApi.DTOs.Grades
 {
     public class GradesQueryDto : QueryDto
     {
-        [JsonPropertyName("firstName")]
         public string? FirstName { get; set; }
-        [JsonPropertyName("lastName")]
         public string? LastName { get; set; }
-        [JsonPropertyName("classSubjectId")]
         public int ClassSubjectId { get; set; } = 1;
-        [JsonPropertyName("gradingYear")]
         public int GradingYear { get; set; } = DateTime.Now.Year;
-        [JsonPropertyName("gradingMonth")]
         public int GradingMonth { get; set; } = DateTime.Now.Month;
     }
 }
--- StudentGradebookApi/DTOs/Grades/StudentGradesBySubjectDTO.cs
+++ StudentGradebookApi/DTOs/Grades/StudentGradesBySubjectDTO.cs
@@ -1,11 +1,18 @@
-﻿namespace StudentGradebookApi.DTOs.Grades
+﻿using System.Text.Json.Serialization;
+
+namespace StudentGradebookApi.DTOs.Grades
 {
     public class StudentGradesBySubjectDto
     {
+        [JsonPropertyName("firstName")]
         public string FirstName { get; set; }
+        [JsonPropertyName("lastName")]
         public string LastName { get; set; }
+        [JsonPropertyName("classSubjectId")]
         public int ClassSubjectId { get; set; }
+        [JsonPropertyName("enrollmentId")]
         public int EnrollmentId { get; set; }
+        [JsonPropertyName("grades")]
         public List<GradesListDto> Grades { get; set; } = new List<GradesListDto>();
     }
 }
